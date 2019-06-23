6d8ffbd0c4b9b394e843c2d19366bb7f6a773e68
Phiiliip <felilotero@gmail.com>
2019-06-23 16:09:49 -0300

    Listar cruceros
    Lista funcionando

--- Vistas.sql
+++ Vistas.sql
@@ -1,7 +1,7 @@
 
 -- Listar todos los cruceros
 
-create view ListarCruceros
+create view LOS_QUE_VAN_A_APROBAR.ListarCruceros
 as
 select C.IdCrucero, M.Descripcion as Marca, Mo.Descripcion as Modelo, C.CantidadCabinas
 from LOS_QUE_VAN_A_APROBAR.Crucero as c
