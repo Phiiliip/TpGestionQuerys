2d28b2f848229f6956b1ac2d6f13d84ea4956d6a
Phiiliip <felilotero@gmail.com>
2019-06-24 18:14:45 -0300

    Super query inicial
    Para crear TODO

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -266,3 +266,425 @@ insert LOS_QUE_VAN_A_APROBAR.FuncionalidadPorRol(IdFuncionalidad,IdRol)
 values (1,2),(2,2),(3,2),(4,2),(5,2),(6,2),(7,2)
 
 
+--
+
+--
+
+-- Vistas
+
+-- Listar todos los cruceros
+
+GO
+create view LOS_QUE_VAN_A_APROBAR.ListarCruceros
+as
+select C.IdCrucero, M.Descripcion as Marca, Mo.Descripcion as Modelo, C.CantidadCabinas
+from LOS_QUE_VAN_A_APROBAR.Crucero as c
+join LOS_QUE_VAN_A_APROBAR.Marca as M on c.IdMarca = M.IdMarca
+join LOS_QUE_VAN_A_APROBAR.Modelo as Mo on c.IdModelo = Mo.IdModelo
+GO
+
+create view LOS_QUE_VAN_A_APROBAR.ListarCrucerosHabilitados
+as
+select C.IdCrucero, M.Descripcion as Marca, Mo.Descripcion as Modelo, C.CantidadCabinas
+from LOS_QUE_VAN_A_APROBAR.Crucero as c
+join LOS_QUE_VAN_A_APROBAR.Marca as M on c.IdMarca = M.IdMarca
+join LOS_QUE_VAN_A_APROBAR.Modelo as Mo on c.IdModelo = Mo.IdModelo
+where c.FechaAlta < SYSDATETIME()
+
+GO
+create view LOS_QUE_VAN_A_APROBAR.ListarCrucerosInhabilitados
+as
+select C.IdCrucero, M.Descripcion as Marca, Mo.Descripcion as Modelo, C.CantidadCabinas
+from LOS_QUE_VAN_A_APROBAR.Crucero as c
+join LOS_QUE_VAN_A_APROBAR.Marca as M on c.IdMarca = M.IdMarca
+join LOS_QUE_VAN_A_APROBAR.Modelo as Mo on c.IdModelo = Mo.IdModelo
+where c.FechaAlta IS NULL
+GO
+
+
+
+-- Procedures
+
+-- LOGIN Y SEGURIDAD:
+
+-- Validacion de Login
+GO
+create procedure LOS_QUE_VAN_A_APROBAR.ValidarAdministrador(@Username NVARCHAR(100), @Password NVARCHAR(100))
+as
+begin
+if exists(select *  from LOS_QUE_VAN_A_APROBAR.Administrador
+where NombreUsuario = @Username and Contraseña = HASHBYTES('SHA2_256', @Password))
+	begin
+	return 1
+	end
+else 
+	begin
+	return 0
+	end
+end
+GO
+
+-- Creacion de administrador
+create procedure LOS_QUE_VAN_A_APROBAR.CrearAdministrador(@Username NVARCHAR(100), @Password NVARCHAR(100))
+as
+begin
+insert LOS_QUE_VAN_A_APROBAR.Administrador(NombreUsuario,Contraseña)
+values (@Username, HASHBYTES('SHA2_256', @Password))
+end
+GO
+--
+
+
+-- ROL
+
+create procedure LOS_QUE_VAN_A_APROBAR.NuevoRol (@NuevoNombre NVARCHAR(20))
+as
+begin
+insert into LOS_QUE_VAN_A_APROBAR.Rol(Nombre)
+values (@Nuevonombre)
+end
+GO
+--
+
+create procedure LOS_QUE_VAN_A_APROBAR.NuevaFuncionalidad(@NuevoNombre NVARCHAR(255))
+as 
+begin
+insert into LOS_QUE_VAN_A_APROBAR.Funcionalidad(Descripcion)
+values(@NuevoNombre)
+end
+GO
+--
+
+create procedure LOS_QUE_VAN_A_APROBAR.FuncionalidadParaRol(@IdRol int, @IdFuncionalidad int)
+as
+begin
+insert into LOS_QUE_VAN_A_APROBAR.FuncionalidadPorRol(IdFuncionalidad, IdRol) 
+values (@IdFuncionalidad, @IdRol)
+end
+GO
+/* Consulta sobre rol y funcionalidad
+select f.Descripcion as Funcionalidad, r.nombre as Rol, fpr.Estado 
+from LOS_QUE_VAN_A_APROBAR.FuncionalidadPorRol as fpr
+join LOS_QUE_VAN_A_APROBAR.Funcionalidad as f on fpr.IdFuncionalidad = f.IdFuncionalidad
+join LOS_QUE_VAN_A_APROBAR.Rol as r on r.IdRol = fpr.IdRol */
+
+--
+create procedure LOS_QUE_VAN_A_APROBAR.ModificarRol(@IdRol int, @Nombre NVARCHAR(20))
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.Rol
+set Nombre = @Nombre
+where IdRol = @IdRol
+end
+GO
+--
+
+create procedure LOS_QUE_VAN_A_APROBAR.BajaRol(@NombreRol NVARCHAR(20))
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.Rol
+set Estado = 'Inhabilitado'
+where Nombre = @NombreRol
+end
+GO
+--
+
+Create procedure LOS_QUE_VAN_A_APROBAR.AltaRol(@IdRol int)
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.Rol
+set Estado = 'Habilitado'
+where IdRol = @Idrol
+end
+GO
+--
+
+create procedure LOS_QUE_VAN_A_APROBAR.BajaFuncionalidadDelRol(@IdRol int, @IdFuncionalidad int)
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.FuncionalidadPorRol
+set Estado = 'Inhabilitado'
+where IdRol = @IdRol and IdFuncionalidad = @IdFuncionalidad
+end
+GO
+--
+
+create procedure LOS_QUE_VAN_A_APROBAR.AltaFuncionalidadDelRol(@IdRol int, @IdFuncionalidad int)
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.FuncionalidadPorRol
+set Estado = 'Habilitado'
+where IdRol = @IdRol and IdFuncionalidad = @IdFuncionalidad
+end
+GO
+
+--
+
+
+
+
+
+
+
+
+-- PUERTO
+
+-- Crear puerto
+create procedure LOS_QUE_VAN_A_APROBAR.CrearPuerto(@NombrePuerto NVARCHAR(255), @Descripcion VARCHAR(50))
+as
+begin
+insert into LOS_QUE_VAN_A_APROBAR.Puerto(Nombre,Descripcion)
+values(@NombrePuerto,@Descripcion)
+end
+GO
+-- Modificar puerto
+create procedure LOS_QUE_VAN_A_APROBAR.ModificarPuerto(@NombrePuerto NVARCHAR(255), @Descripcion VARCHAR(50))
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.Puerto
+set Descripcion = @Descripcion
+where Nombre = @NombrePuerto
+end
+
+GO
+
+-- Dar De baja
+GO
+create Procedure LOS_QUE_VAN_A_APROBAR. EliminarPuerto(@NombrePuertoBorrado NVARCHAR(255)) 
+as 
+begin
+declare @IdRecorrido int
+declare @IdTramo int
+declare MiCursor CURSOR for
+select CodigoRecorrido, IdTramo from LOS_QUE_VAN_A_APROBAR.RecorridoPorTramo r
+join LOS_QUE_VAN_A_APROBAR.Tramo t on t.IdTramo = r.CodigoTramo
+where Puerto_Llegada = @NombrePuertoBorrado or Puerto_Salida = @NombrePuertoBorrado
+
+open MiCursor 
+fetch next from MiCursor into @IdRecorrido, @IdTramo
+
+while @@FETCH_STATUS = 0
+
+BEGIN
+
+delete from LOS_QUE_VAN_A_APROBAR.Reserva
+where IdViaje in(select IdViaje from Viaje v
+where IdRecorrido = @IdRecorrido)
+
+delete from LOS_QUE_VAN_A_APROBAR.Pasaje
+where IdViaje in(select IdViaje from Viaje v
+where IdRecorrido = @IdRecorrido)
+
+delete from LOS_QUE_VAN_A_APROBAR.RecorridoPorTramo 
+where CodigoRecorrido = @IdRecorrido
+
+delete from LOS_QUE_VAN_A_APROBAR.Viaje
+where IdViaje in (select IdViaje from Viaje v
+where IdRecorrido = @IdRecorrido)
+
+delete from LOS_QUE_VAN_A_APROBAR.Recorrido
+where IdRecorrido = @IdRecorrido
+
+fetch next from MiCursor into @IdRecorrido, @IdTramo
+
+END
+
+close MiCursor
+deallocate MiCursor
+
+delete from LOS_QUE_VAN_A_APROBAR.Tramo
+where Puerto_Salida = @NombrePuertoBorrado OR Puerto_Llegada = @NombrePuertoBorrado
+
+delete from LOS_QUE_VAN_A_APROBAR.Puerto 
+where Nombre = @NombrePuertoBorrado
+
+END
+GO
+--
+
+
+
+
+
+
+
+-- Recorrido
+
+Create procedure LOS_QUE_VAN_A_APROBAR.CrearRecorrido(@CodigoRecorrido decimal(18,2), @Descripcion varchar(50), @Precio decimal(18,2))
+AS
+BEGIN
+INSERT INTO LOS_QUE_VAN_A_APROBAR.Recorrido(Codigo_Recorrido, Descripcion, PrecioTotal)
+VALUES (@CodigoRecorrido, @Descripcion, @Precio)
+END
+GO
+--Crear tramo de recorrido
+
+CREATE PROCEDURE LOS_QUE_VAN_A_APROBAR.InsertarTramoDeRecorrido(@CodigoRecorrido int, @CodigoTramo int, @Precio decimal(18,2))
+AS
+BEGIN
+INSERT INTO LOS_QUE_VAN_A_APROBAR.RecorridoPorTramo(CodigoRecorrido, CodigoTramo, PrecioTramo)
+values( @CodigoRecorrido, @CodigoTramo, @Precio)
+END
+GO
+--modificar tramo de recorrido
+
+Create PROCEDURE LOS_QUE_VAN_A_APROBAR.modificarTramoDeRecorrido(@IdRecorrido int,@TramoViejo int, @TramoNuevo int)
+AS
+BEGIN
+
+update LOS_QUE_VAN_A_APROBAR.RecorridoPorTramo
+set CodigoTramo = @TramoNuevo
+WHERE CodigoRecorrido = @IdRecorrido AND CodigoTramo = @TramoViejo
+
+end
+GO
+
+
+--DAR DE BAJA RECORRIDO
+CREATE PROCEDURE LOS_QUE_VAN_A_APROBAR.BajaRecorrido(@IdRecorrido int)
+AS
+BEGIN
+
+IF NOT EXISTS (SELECT R.IdRecorrido
+FROM LOS_QUE_VAN_A_APROBAR.Pasaje p
+ JOIN LOS_QUE_VAN_A_APROBAR.Viaje v ON (v.IdViaje = p.IdViaje)
+ JOIN LOS_QUE_VAN_A_APROBAR.Recorrido r ON (v.IdRecorrido = r.IdRecorrido)
+WHERE p.Fecha_Salida > GETDATE() AND r.IdRecorrido = @IdRecorrido
+)
+
+BEGIN
+UPDATE LOS_QUE_VAN_A_APROBAR.Recorrido
+SET  Estado = 'Inhabilitado'
+WHERE IdRecorrido = @IdRecorrido
+END
+
+ELSE
+BEGIN
+PRINT 'Hay pasajes vendidos para un viaje que todavía no se realizó'
+END
+
+END
+GO
+
+
+
+
+
+
+--
+
+
+
+-- CRUCERO
+
+-- Creacion de un crucero
+
+create procedure LOS_QUE_VAN_A_APROBAR.AltaCrucero(@IdCrucero NVARCHAR(50), @IdMarca int, @IdModelo int, @CantidadCabinas int)
+as
+begin
+insert LOS_QUE_VAN_A_APROBAR.Crucero(IdCrucero,IdMarca,IdModelo,FechaAlta,CantidadCabinas)
+values(@IdCrucero,@IdMarca,@IdModelo,SYSDATETIME(), @CantidadCabinas)
+end
+GO
+-- Modificar Marca de crucero
+
+create procedure LOS_QUE_VAN_A_APROBAR.ModificarMarca(@IdCrucero NVARCHAR(50), @IdMarca int)
+as 
+begin
+update LOS_QUE_VAN_A_APROBAR.Crucero
+set IdCrucero = @IdCrucero, IdMarca = @IdMarca
+end
+GO
+
+-- Modificar Modelo de crucero
+
+create procedure LOS_QUE_VAN_A_APROBAR.ModificarModelo(@IdCrucero NVARCHAR(50), @IdModelo int)
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.Crucero
+set IdCrucero = @IdCrucero, IdModelo = @IdModelo
+end
+GO
+-- Baja definitiva de crucero
+
+create procedure LOS_QUE_VAN_A_APROBAR.BajaDefinitiva(@IdCrucero NVARCHAR(50))
+as 
+begin
+update LOS_QUE_VAN_A_APROBAR.Crucero
+set FechaAlta = NULL
+where IdCrucero = @IdCrucero
+
+insert LOS_QUE_VAN_A_APROBAR.FueraDeServicio(IdCrucero,FechaBaja,MotivoBaja)
+values(@IdCrucero, SYSDATETIME(), 'Vida util finalizada')
+
+end
+GO
+-- Baja fuera de servicio
+
+create procedure LOS_QUE_VAN_A_APROBAR.BajaFueraDeServicio(@IdCrucero NVARCHAR(50), @FechaDeAlta datetime2(3))
+as
+begin
+update LOS_QUE_VAN_A_APROBAR.Crucero
+set FechaAlta = @FechaDeAlta
+where IdCrucero = @IdCrucero
+
+insert LOS_QUE_VAN_A_APROBAR.FueraDeServicio(IdCrucero, FechaBaja, MotivoBaja, FechaReinicio)
+values(@IdCrucero,SYSDATETIME(), 'Fuera de servicio', @FechaDeAlta)
+
+end
+GO
+
+
+--
+
+
+
+
+
+
+
+
+
+-- VIAJE
+
+-- Creacion de un viaje
+
+create procedure LOS_QUE_VAN_A_APROBAR.CrearViaje(@IdCrucero nvarchar(50), @IdRecorrido int, @Fecha_Salida datetime2(3), @Fecha_Llegada datetime2(3), @CodigoRecorrido decimal(18,0))
+as
+begin
+insert LOS_QUE_VAN_A_APROBAR.Viaje(IdCrucero,IdRecorrido,Fecha_Salida,Fecha_Llegada,CodigoRecorrido)
+values(@IdCrucero,@IdRecorrido,@Fecha_Salida, @Fecha_Llegada, @CodigoRecorrido)
+end
+GO
+
+
+
+
+-- Funciones
+
+
+create function LOS_QUE_VAN_A_APROBAR.ObtenerNuevoRolInsertado()
+returns int as
+begin
+return (select TOP(1) IdRol from LOS_QUE_VAN_A_APROBAR.Rol order by IdRol DESC)
+end
+GO
+
+
+-- Triggers
+
+-- ROL
+
+-- Cuando se inhabilita un rol, todos los usuarios con ese rol pierden su rol
+
+create trigger LOS_QUE_VAN_A_APROBAR.SacarRolesDeUsuario on LOS_QUE_VAN_A_APROBAR.Rol after update
+as
+begin
+update c  
+set IdRol = NULL
+from LOS_QUE_VAN_A_APROBAR.Cliente as c
+join Rol as r on r.IdRol = c.IdRol
+where r.Estado = 'Inhabilitado'
+end
+GO
+
