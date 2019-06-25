ccc502ee9786e5ba4a082d905a7bcaadfc477c22
Phiiliip <felilotero@gmail.com>
2019-06-24 22:45:25 -0300

    Update zVistas.sql

--- zVistas.sql
+++ zVistas.sql
@@ -26,3 +26,16 @@ join LOS_QUE_VAN_A_APROBAR.Marca as M on c.IdMarca = M.IdMarca
 join LOS_QUE_VAN_A_APROBAR.Modelo as Mo on c.IdModelo = Mo.IdModelo
 where c.FechaAlta IS NULL
 GO
+
+select r.CodigoRecorrido, p1.Nombre, p2.Nombre
+from LOS_QUE_VAN_A_APROBAR.RecorridoPorTramo as r
+join LOS_QUE_VAN_A_APROBAR.Tramo t on r.CodigoTramo = t.IdTramo
+join LOS_QUE_VAN_A_APROBAR.Puerto p1 on t.Puerto_Llegada = p1.Nombre
+join LOS_QUE_VAN_A_APROBAR.Puerto p2 on t.Puerto_Salida = p2.Nombre
+
+
+-- Recorridos
+
+create view LOS_QUE_VAN_A_APROBAR.BajaRecorrido
+as
+select (
