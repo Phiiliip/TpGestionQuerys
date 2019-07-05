536c791d28051a0693bf7d2daf77d56b960460ff
EmaSedlar <38228357+EmaSedlar@users.noreply.github.com>
2019-07-05 14:55:34 -0300

    Funcion nueva agregada

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -1048,6 +1048,44 @@ GO
 
 ------------------------------ Funciones ------------------------------
 
+---Cantidad dias fuera de servicio--
+
+create function LOS_QUE_VAN_A_APROBAR.diasFuera (@anio numeric(5), @semestre numeric(2), @fechabaja dateTime, @fechaalta dateTime)
+returns numeric(5)
+as
+begin
+	declare @resultado numeric(5)
+	
+	declare @fechalimite1 dateTime
+	declare @fechalimite2 dateTime
+	declare @fechalimite3 dateTime
+	set @fechalimite1 = DATEFROMPARTS(@anio, 1, 1)
+	set @fechalimite2 = DATEFROMPARTS(@anio, 6, 30)
+	set @fechalimite3 = DATEFROMPARTS(@anio, 12, 31)
+
+	if(@semestre = 1)
+	begin
+		if(@fechabaja < @fechalimite1)
+			set @fechabaja = @fechalimite1
+		if(@fechaalta > @fechalimite2)
+			set @fechaalta = @fechalimite2
+		if(@fechabaja > @fechalimite2 or @fechaalta < @fechalimite1)
+			set @fechabaja = @fechaalta
+	end
+	else
+	begin
+		if(@fechabaja < @fechalimite2)
+			set @fechabaja = @fechalimite2
+		if(@fechaalta > @fechalimite3)
+			set @fechaalta = @fechalimite3
+		if(@fechabaja > @fechalimite3 or @fechaalta < @fechalimite2)
+			set @fechabaja = @fechaalta
+	end
+
+	set @resultado = DATEDIFF(day, @fechabaja, @fechaalta)
+	return @resultado
+end
+
 
 
 
